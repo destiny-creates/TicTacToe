340c41b73d69f868a54df870693a4459a4a7f14e
destiny creates <dronesforaliving@gmail.com>
2018-07-20 13:12:01 -0400

    set up images and first button

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    
+    var isXturn = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func button1tapped(_ sender: Any) {
+        if isXturn {
+            button1.setBackgroundImage(#imageLiteral(resourceName: "x"), for: .normal)
+        } else {
+            button1.setBackgroundImage(#imageLiteral(resourceName: "o"), for: .normal)
+        }
+        isXturn = !isXturn
+        button1.isEnabled = false
+    }
+    @IBAction func button2tapped(_ sender: Any) {
+        print("button 2 tapped")
+    }
+    @IBAction func button3tapped(_ sender: Any) {
+        print("button 3 tapped")
+    }
+    @IBAction func button4tapped(_ sender: Any) {
+        print("button 4 tapped")
+    }
+    @IBAction func button5tapped(_ sender: Any) {
+        print("button 5 tapped")
+    }
+    @IBAction func button6tapped(_ sender: Any) {
+        print("button 6 tapped")
+    }
+    @IBAction func button7tapped(_ sender: Any) {
+        print("button 7 tapped")
+    }
+    @IBAction func button8tapped(_ sender: Any) {
+        print("button 8 tapped")
+    }
+    @IBAction func button9tapped(_ sender: Any) {
+        print("button 9 tapped")
+    }
+    
 }
 
